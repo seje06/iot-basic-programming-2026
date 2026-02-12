00bf8a21cd7cbb4596009a0652176050f2a2e15c
leechunghyun7 <as05100510@gmail.com>
2026-02-12 12:32:09 +0900

    day 9 12:32

---
+++ Day09/Chap18-Solution/Chap18-01-app/main.c
@@ -0,0 +1,49 @@
+﻿// 파일 입출력 + 프로그램 실행경로
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <direct.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[256]; // 경로 입력 변수
+
+	_getcwd(str, sizeof(str));
+	printf("현재 작업 폴더 : %s\n", str);
+	
+	FILE* fp, * ofp;
+
+	// 디렉토리, 폴더 구분자로 /를 사용해도 무방
+	fp = fopen("sample.txt", "r");
+	if (fp == NULL)
+	{
+		printf("파일 열기 실패\n");
+		exit(1);
+	}
+	else
+	{
+		printf("파일 오픈 성공\n");
+	}
+
+	// 파일 복사용 오픈
+	ofp = fopen("copy.txt", "w");
+	if (ofp == NULL) // 실패 방지
+	{
+		printf("출력파일 열기 실패\n");
+		return 1;
+	}
+
+	while (fgets(str, sizeof(str), fp) != NULL)
+	{
+		printf("한줄 쓰기 -> %s", str);
+		str[strlen(str) - 1]  ='\0'; // \n -> \0 바꿔줘서
+		fputs(str, ofp);
+		fputs(" ", ofp); // 한줄 끝난뒤 스페이스 추가
+	}
+
+	fclose(ofp);
+	fclose(fp);
+
+	return 0;
+}
---
+++ Day09/Chap18-Solution/Chap18-02-app/main.c
@@ -0,0 +1,53 @@
+﻿// 파일 입출력2
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <windows.h> // OS관련 또는 코드페이지 인코딩 해결
+
+int main(void)
+{
+	//콘솔출력, 입력을 UTF-8(65001)로 고정설정
+	SetConsoleOutputCP(65001); // 콘솔출력 코드페이지
+	SetConsoleCP(65001); // 콘솔자체 코드페이지
+
+	FILE* ifp, * ofp;
+
+	// 텍스트 내 포맷에 맞춰서 변수 선언
+	char name[20];
+	int kor, eng, math; //국, 영, 수 점수
+	int total;
+	double avg;
+	int res;
+	char str[256];
+
+	ifp = fopen("original.txt", "r"); // r모드는 파일이 없으면 실패
+	if (ifp == NULL)
+	{
+		printf(u8"입력 파일 오픈 실패!\n");
+		exit(1);
+	}
+
+	ofp = fopen("./copy.txt", "w");// w쓰기 모드는 파일이 없으면 파일 생성
+	if (ofp == NULL)
+	{
+		printf(u8"출력 파일 생성 실패!\n");
+		exit(1);
+	}
+
+	// 입력 파일포인터에서 읽어서 한줄씩 각 변수에 할당
+	while (fscanf(ifp, "%s%d%d%d", name, &kor, &eng, &math) != EOF)
+	{
+		total = kor + eng + math;
+		avg = total / 3.0;
+
+		printf(u8"%s%5d%7.1lf\n", name, total, avg); // 콘솔에 출력
+		fprintf(ofp, "%s%5d%7.1lf\n", name, total, avg);
+	}
+
+	printf(u8"파일 복사 완료\n");
+
+	fclose(ifp);
+	fclose(ofp);
+	return 0;
+}
